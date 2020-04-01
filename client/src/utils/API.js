7681e8072e37f25e0c6167b71ab6f2a183fa4057
ccostoso <carlos.costoso@outlook.com>
2020-04-01 17:02:02 -0400

    Enhanced security by creating config.js file to store API key and adding file to .gitignore

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,9 +1,10 @@
 import axios from "axios";
+import config from "./config";
 
 export default {
     // Gets all books
     searchBooks: function (searchValue) {
-        return axios.get(`https://www.googleapis.com/books/v1/volumes?q=${searchValue}&key=AIzaSyCA7K2OiqwryC4H6PzM8jannybljBnjrI8`);
+        return axios.get(`https://www.googleapis.com/books/v1/volumes?q=${searchValue}&key=${config.apiKey}`);
     },
     getBooks: function () {
         return axios.get("/api/books");
