1ee8919fc245a2cf5bf5f680e7948965aad73b59
ccostoso <carlos.costoso@outlook.com>
2020-04-28 20:51:54 -0400

    Heroku fixes pt. 2

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require("express");
+require("dotenv").config();
 
 const mongoose = require("mongoose");
 const routes = require("./routes");
 const app = express();
 const PORT = process.env.PORT || 3001;
+console.log(process.env);
 
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
